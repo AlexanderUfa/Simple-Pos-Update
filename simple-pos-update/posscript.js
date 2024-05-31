81f6471ccaea2dcc83dbec9894bdac456affac2b
AlexanderUfa <75223388+AlexanderUfa@users.noreply.github.com>
2024-05-31 19:15:26 +0500

    Update posscript.js

--- simple-pos-update/posscript.js
+++ simple-pos-update/posscript.js
@@ -1,1 +1,31 @@
+(function ($) {
+    
+$(document).ready(function ()
+{
+ 
+    $(".vpos_ajax").on('dblclick', function ()
+    {
+        var position = $(this).val();
+        var postid = $(this).data('postid');
+        
+        var obj = this;
+        
+            $(obj).hide();
+        
+            
+            $.post(ajaxurl, { 'action': 'posupdate', 'postid':postid, 'position':position }, function (data) 
+            {
+                   
+                 //alert(data);
+                 $(obj).show();
+              
+            });
+        
+    });
+    
+
+});
+
+})(jQuery);
+
 
