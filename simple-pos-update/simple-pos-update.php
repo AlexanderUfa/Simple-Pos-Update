854f41cdfc70811743f113abdad6e088648e0dde
AlexanderUfa <75223388+AlexanderUfa@users.noreply.github.com>
2024-06-01 11:21:35 +0500

    Update simple-pos-update.php
    Теперь столбец позиция добавляется ко всем типам постов

--- simple-pos-update/simple-pos-update.php
+++ simple-pos-update/simple-pos-update.php
@@ -14,33 +14,57 @@ final class SimplePosUpdate
      
      public function __construct() 
      {
-         // создаем новую колонку
-         add_filter( 'manage_' . 'post' . '_posts_columns', array( $this, 'add_views_column' ) , 4 );
-         
-         // заполняем колонку данными
-         add_action( 'manage_' . 'post' . '_posts_custom_column', array( $this, 'fill_views_column' ), 5, 2 );
-         
-         // добавляем возможность сортировать колонку
-         add_filter( 'manage_' . 'edit-post' . '_sortable_columns', array( $this, 'add_views_sortable_column' ));
-         
-         //подключаем скрипты и стили
-         add_action( 'admin_enqueue_scripts', array( $this , 'true_include_myscript') , 25 );
-         
-          // изменяем запрос при сортировке колонки
-         add_filter( 'request', array( $this , 'add_column_views_request' )   );
+         /*добавляем колонки ко всем типам постов. get_current_screen() работает только после хука current_screen*/
+         add_action( 'current_screen', array( $this, 'add_columns' ) );
          
          //запрос на обновление позиции
          add_action( 'wp_ajax_posupdate', array( $this , 'vin_posupdate' ) );
          add_action( 'wp_ajax_nopriv_posupdate', array( $this , 'vin_posupdate' )  );
          
-         
-          
-       
      }
+     
+     
+     public function add_columns()
+     {
+         $screen = (array) get_current_screen();
+         $post_type = $screen['post_type'];
+         $base = $screen['base'];
+         
+         //html::pre($screen);
+
+         if ( $base == 'edit' ) 
+         {
+              // создаем новую колонку
+             add_filter( 'manage_' . $post_type . '_posts_columns', array( $this, 'add_views_column' ) , 4 );
+         
+            // заполняем колонку данными
+            add_action( 'manage_' . $post_type . '_posts_custom_column', array( $this, 'fill_views_column' ), 5, 2 );
+         
+            // добавляем возможность сортировать колонку
+             add_filter( 'manage_' . 'edit-'. $post_type . '_sortable_columns', array( $this, 'add_views_sortable_column' ));
+             
+             //подключаем скрипты и стили
+             add_action( 'admin_enqueue_scripts', array( $this , 'true_include_myscript') , 25 );
+            // изменяем запрос при сортировке колонки
+             add_filter( 'request', array( $this , 'add_column_views_request' )   );
+             
+             
+         }
+         
+         
+         
+        
+         
+         
+         
+     }
+     
+     
      
      // создаем новую колонку
      public function add_views_column( $columns )
         {
+	 
 
 	$new_columns = [
 		SimplePosUpdate::posFieldName  => 'Позиция',
