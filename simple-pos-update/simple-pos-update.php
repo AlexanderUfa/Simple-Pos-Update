85c57fc8d498b37f68ac3902bc01467e5af1fa53
AlexanderUfa <75223388+AlexanderUfa@users.noreply.github.com>
2024-06-03 11:48:59 +0500

    Update simple-pos-update.php
    Англоязычные комментарии

--- simple-pos-update/simple-pos-update.php
+++ simple-pos-update/simple-pos-update.php
@@ -13,20 +13,24 @@ License: GPL3
 
 final class SimplePosUpdate
 {
-     /*имя столбца и произвольного поля поста по которому сортировка и вывод*/
+     /* имя столбца и произвольного поля поста по которому сортировка и вывод */
+     /* name of the column and custom field of the post by which sorting and output */
      const posFieldName = 'pos';
     
      
      public function __construct() 
      {
           
-         /*запускаем плагин только в админке*/
+         /* запускаем плагин только в админке */
+         /* run the plugin only in the admin panel */
          if ( is_admin() ) 
          {
             /*добавляем колонки ко всем типам постов. get_current_screen() работает только после хука current_screen*/
+            /*add columns to all post types. get_current_screen() only works after the current_screen hook*/
             add_action( 'current_screen', array( $this, 'add_columns' ) );
          
-            //запрос на обновление позиции
+            //ajax запрос на обновление позиции
+            //ajax request to update position
             add_action( 'wp_ajax_posupdate', array( $this , 'vin_posupdate' ) );
             add_action( 'wp_ajax_nopriv_posupdate', array( $this , 'vin_posupdate' )  );
              
@@ -45,18 +49,24 @@ final class SimplePosUpdate
 
          if ( $base == 'edit' ) 
          {
-              // создаем новую колонку
+            // создаем новую колонку
+            // create a new column
             add_filter( 'manage_' . $post_type . '_posts_columns', array( $this, 'add_views_column' ) , 4 );
          
             // заполняем колонку данными
+            //fill the column with data
             add_action( 'manage_' . $post_type . '_posts_custom_column', array( $this, 'fill_views_column' ), 5, 2 );
          
             // добавляем возможность сортировать колонку
+            // add the ability to sort the column
             add_filter( 'manage_' . 'edit-'. $post_type . '_sortable_columns', array( $this, 'add_views_sortable_column' ));
              
-             //подключаем скрипты и стили
+            //подключаем скрипты и стили
+            //connect scripts and styles
             add_action( 'admin_enqueue_scripts', array( $this , 'true_include_myscript') , 25 );
+            
             // изменяем запрос при сортировке колонки
+            // change the query when sorting the column
             add_filter( 'request', array( $this , 'add_column_views_request' )   );
              
              
@@ -67,6 +77,7 @@ final class SimplePosUpdate
      
      
      // создаем новую колонку
+     // create a new column
      public function add_views_column( $columns )
         {
 	
@@ -77,7 +88,8 @@ final class SimplePosUpdate
         }
  
         
-      // заполняем колонку данными  
+      // заполняем колонку данными   
+      // fill the column with data  
      public function fill_views_column( $colname, $post_id ) 
      {
 	if( $colname === SimplePosUpdate::posFieldName )
@@ -93,6 +105,7 @@ final class SimplePosUpdate
      
 
         // добавляем возможность сортировать колонку
+        // add the ability to sort the column
         public function add_views_sortable_column($sortable_columns) {
             
             $sortable_columns[ SimplePosUpdate::posFieldName ] = SimplePosUpdate::posFieldName;
@@ -103,6 +116,7 @@ final class SimplePosUpdate
 
 
         //подключаем скрипты и стили
+        //connect scripts and styles
         public function true_include_myscript() 
         {
             
@@ -112,6 +126,7 @@ final class SimplePosUpdate
         }
         
          // изменяем запрос при сортировке колонки
+        // change the query when sorting the column
         public function add_column_views_request( $vars ) 
         {
 	if( isset( $vars['orderby'] ) && $vars['orderby'] === SimplePosUpdate::posFieldName ){
@@ -123,7 +138,9 @@ final class SimplePosUpdate
         }
         
         
-
+        //обработка ajax запроса на обновление позиции поста
+        //processing an ajax request to update a post position
+        
         public function vin_posupdate() 
         {
         $position = intval( $_POST['position'] );
@@ -137,7 +154,8 @@ final class SimplePosUpdate
 
 
          
-         
+     //возвращает url до папки плагина    
+     //returns the url to the plugin folder
     public static function getUrlPath() 
      {
          return plugins_url( basename( __FILE__ , '.php' ) );
